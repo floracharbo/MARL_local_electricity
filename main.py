8162e8d866dfe5ef4139c2d719d3161af4f7efb0
floracharbo <flora.charbonnier@live.fr>
2023-02-06 14:15:25 +0000

    2 states

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from src.simulations.runner import run
 
 settings = {
     'RL': {
-       'state_space': [['grdC']],
+       'state_space': [['grdC', 'flexibility']],
        'n_epochs': 5,
        'n_repeats': 2,
        'type_learning': ['DDPG'],
