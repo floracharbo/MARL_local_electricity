8e25c625b48b37ef9a57d186fdcffaab1d2bdb98
floracharbo <flora.charbonnier@live.fr>
2023-02-06 14:25:11 +0000

    update state_batch = tf.reshape(state_batch, (-1, 1, self.rl['dim_states'])) input target_actor

--- src/learners/DDPG.py
+++ src/learners/DDPG.py
@@ -170,7 +170,7 @@ class Buffer:
             )
             action_batch = tf.reshape(action_batch, (shape[0], 1, shape[1]))
         else:
-            state_batch = tf.reshape(state_batch, (-1, self.rl['dim_states']))
+            state_batch = tf.reshape(state_batch, (-1, 1, self.rl['dim_states']))
         with tf.GradientTape() as tape:
             target_actions = target_actor(next_state_batch, training=True)
             if self.rl['LSTM']:
