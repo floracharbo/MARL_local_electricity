030cebc4d342e27be4a801bc1437f1bd7abbaa4d
floracharbo <flora.charbonnier@live.fr>
2023-03-03 09:44:25 +0100

    plan runs

--- main.py
+++ main.py
@@ -14,26 +14,32 @@ from src.simulations.runner import run
 
 # Enter experiment-specific settings in the dictionary below if using different parameters
 # to the default parameters in config_files/default_input_parameters, using the example syntax below.
-
+q learning / facmac no traj / facmac traj / facmac supervised no traj / facmac supervised traj
 settings = {
     'RL': {
-       'state_space': [['grdC_n2', 'flexibility'], ['grdC']],
-       'n_epochs': 20,
-       'n_repeats': 3,
-       'type_learning': ['facmac'] * 2,
-       'evaluation_methods': [['env_r_c', 'opt_r_c']] * 2,
-       'facmac': {
-           'hysteretic': True,
-           'beta_to_alpha': 0.1,
-        },
+        'state_space': [['flexibility', 'grdC_n2']] * 6 * 5,
+        'n_epochs': 20,
+        'n_repeats': 3,
+        'type_learning': ['q_learning'] * 6 + ['facmac'] * 6 * 4,
+        'trajectory': [False] * 6 * 2 + [True] * 6 + [False] * 6 + [True] * 6,
+        'supervised_loss': [False] * 6 * 3 + [True] * 6 * 2,
+        'act_noise': 0.01,
+        'lr': 1e-2,
     },
     'syst': {
-       'n_homes': 10,
+        'force_optimisation': True,
+        'N': [1, 3, 5, 10, 20, 50] * 5,
     }
 }
+trajectory = settings['RL']['trajectory']
+settings['RL']['evaluation_methods'] = None if settings['RL']['type_learning'] == 'q_learning' else 'env_r_c'
+settings['RL']['obs_agent_id'] = False if trajectory else True
+settings['RL']['nn_type'] = 'cnn' if trajectory else 'linear'
+settings['RL']['rnn_hidden_dim'] = 1e3 if trajectory else 5e2
 
 # 1 to run simulation, 2 to plot runs in no_runs, 3 plots results vs n_ag
 RUN_MODE = 1
-no_runs = [823]  # if plotting
+no_runs = [282]  # if plotting
 
 run(RUN_MODE, settings, no_runs)
+
