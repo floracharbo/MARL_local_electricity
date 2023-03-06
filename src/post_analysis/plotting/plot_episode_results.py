Correct actions for baseline and if oppsoite sign of storage

src/post_analysis/plotting/plot_episode_results.py
@@ -977,7 +977,7 @@ def map_over_undervoltage(
                     voltage = np.sqrt(last['voltage_squared'][method][prm["syst"]["N"] - 1][bus])
                     x, y = net.bus_geodata.loc[bus, ["x", "y"]]
                     plot.plt.annotate(
-                        f"Generation bus, \n Voltage is {round(voltage, 3)} p.u."
+                        f"Load bus, \n Voltage is {round(voltage, 3)} p.u."
                         f"\n Q_house is {round(q_house, 3)} kVAR"
                         f"\n Q_car is {round(q_car, 3)} kVAR",
                         xy=(x, y), xytext=(x + 5, y + 5), fontsize=10)

src/simulations/action_translator.py
@@ -749,17 +749,16 @@ class Action_translator:
         no_flex_actions = self._get_no_flex_actions('flexible_q_car_action')
         flexible_q_car_actions = np.zeros(self.n_homes)
         for home in range(self.n_homes):
+            active_power = max(res['charge'][:, time_step], abs(res['discharge_other'][:, time_step]))
             if res['q_car_flex'][home, time_step] > 1e-3:
-                charge = res['charge'][:, time_step]
-                max_q_car_import_flexibility = np.sqrt(self.max_apparent_power_car**2 - charge**2)
+                max_q_car_import_flexibility = np.sqrt(self.max_apparent_power_car**2 - active_power**2)
                 flexible_q_car_actions[home] = (
                     res['q_car_flex'][home, time_step] - self.min_q_car_import
                 ) / (max_q_car_import_flexibility[home] - self.min_q_car_import)
             # if some discharge flex is used, reactive power export available
             elif res['q_car_flex'][home, time_step] > 1e-3:
-                discharge = res['discharge_other'][home, time_step]
                 max_q_car_export_flexibility = - np.sqrt(
-                    self.max_apparent_power_car**2 - discharge**2
+                    self.max_apparent_power_car**2 - active_power**2
                 )
                 flexible_q_car_actions[home] = \
                     - abs(self.min_q_car_export - res['q_car_flex'][home, time_step]) \