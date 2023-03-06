5b681379135b06ff790fd475abcc20a8a539c111
floracharbo <flora.charbonnier@live.fr>
2023-03-06 23:11:18 +0100

    tidy

--- src/learners/facmac/controllers/basic_controller.py
+++ src/learners/facmac/controllers/basic_controller.py
@@ -10,18 +10,19 @@ from src.learners.facmac.modules.agents import REGISTRY as agent_REGISTRY
 # This multi-agent controller shares parameters between agents
 class BasicMAC:
     def __init__(self, scheme, groups, rl, N):
-        self.n_homes = rl['n_homes']
         self.rl = rl
+        for attribute in [
+            'agent_output_type', 'n_homes', 'n_homes_test'
+        ]:
+            setattr(self, attribute, rl[attribute])
         input_shape = self._get_input_shape(scheme)
         self.N = N
         self._build_agents(input_shape)
-        for attribute in ['agent_output_type', 'action_selection_its', 'state_exec_to_train', 'action_train_to_exec']:
-            setattr(self, attribute, rl[attribute])
         self.hidden_states = None
         self.epsilon = rl['facmac']['epsilon0'] if rl['facmac']['epsilon_decay'] else rl['facmac']['epsilon']
 
     def select_actions(self, ep_batch, t_ep, t_env, bs=slice(None),
-                       test_mode=False, explore=False):
+                       test_mode=False, explore=False, ext=None):
         # Only select actions for the selected batch elements in bs
         avail_actions = ep_batch["avail_actions"][:, t_ep]
         rdn_eps = np.random.rand()
@@ -33,7 +34,8 @@ class BasicMAC:
 
             chosen_actions = self.action_selector.select_action(
                 agent_outputs[bs], avail_actions[bs], t_env,
-                test_mode=test_mode, explore=explore)
+                test_mode=test_mode, explore=explore
+            )
 
         return chosen_actions
 
