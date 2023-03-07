b5ca9e52d52fc5954195d475cabadf89b6f2eb1b
floracharbo <flora.charbonnier@live.fr>
2023-03-07 11:19:08 +0000

    update list results

--- src/post_analysis/log/plot_facmac_paper.py
+++ src/post_analysis/log/plot_facmac_paper.py
@@ -11,15 +11,16 @@ from scipy import optimize
 font = {'size': 14}
 matplotlib.rc('font', **font)
 
+compare_times = ['OMQ', 'FH']
 labels = {
     'IQ': 'Independent Q-learning',
     'OIQ': 'Optimisation-informed independent Q-learning',
     'MQ': 'Marginal reward Q-learning',
     'OMQ': 'Optimisation-informed, marginal reward Q-learning',
     # 'FD': 'FACMAC (day-ahead)',
-    # 'FDO': 'Optimisation-informed  FACMAC (day-ahead)',
+    'FDO': 'Optimisation-informed  FACMAC (day-ahead)',
     'FH': 'FACMAC',  # (hourly)
-    'FHO': 'Optimisation-informed FACMAC (hourly)'
+    # 'FHO': 'Optimisation-informed FACMAC (hourly)'
 }
 type_learning = {
     'IQ': 'env_r_d',
@@ -44,23 +45,33 @@ opt_informed = {
 }
 runs = {
     # 'OMQ': [170, 94, 96, 97, 98, 99],
-    'OMQ': [236, 237, 238, 234,  240, 244],
+    # 'OMQ': [236, 237, 238, 234,  240, 244],
+    'OMQ': [420, 421, 422, 423, 424, 509],
 
     # 'IQ': [170, 94, 96, 97, 98, 99],
-    'IQ': [236, 237, 238, 234,  240, 244],
+    # 'IQ': [236, 237, 238, 234,  240, 244],
+    'IQ': [420, 421, 422, 423, 424, 509],
     # 'OIQ': [170, 94, 96, 97, 98, 99],
     # 'MQ': [170, 94, 96, 97, 98, 99],
-    'OIQ': [236, 237, 238, 234,  240, 244],
-    'MQ': [236, 237, 238, 234,  240, 244],
+    # 'OIQ': [236, 237, 238, 234,  240, 244],
+    'OIQ': [420, 421, 422, 423, 424, 509],
+    # 'MQ': [236, 237, 238, 234,  240, 244],
+    'MQ': [420, 421, 422, 423, 424, 509],
 
     # 'FD': [160, 105, 106, 123, 124, 125],
-    'FD': list(range(265, 271)),
-    'FDO': list(range(257, 261)) + list(range(263, 265)),
+    # 'FD': list(range(265, 271)),
+    'FD': list(range(516, 522)),
+
+    # 'FDO': list(range(257, 261)) + list(range(263, 265)),
+    'FDO': list(range(528, 534)),
 
     # 'FH': [126, 127, 128, 129, 130, 131],
-    'FH': list(range(245, 251)),
+    # 'FH': list(range(245, 251)),
+    'FH': list(range(510, 516)),
+
     # 'FHO': [161, 162, 163, 164, 168, 169]
-    'FHO': list(range(251, 257)),
+    # 'FHO': list(range(251, 257)),
+    'FHO': list(range(522, 528)),
 }
 
 
@@ -89,7 +100,7 @@ colours = {
     'MQ': green,
     'OMQ': green,
     'FD': grey,
-    'FDO': grey,
+    'FDO': red,
     'FH': red,
     'FHO': red,
 }
@@ -130,7 +141,7 @@ def logx(x, a, b):
     return a * np.log(x) + b
 
 x_max = n_homes[-1]
-ys_time = {entry: [] for entry in ['OMQ', 'FH']}
+ys_time = {entry: [] for entry in compare_times}
 
 fig, axs = plt.subplots(2, 1, figsize=(8, 10))
 
@@ -161,7 +172,7 @@ for line_label in labels.keys():
         alpha=0.2,
         color=colour
     )
-    if line_label in ['OMQ', 'FH']:
+    if line_label in compare_times:
         axs[1].plot(
             n_homes, time_end, 'o',
             color=colour
@@ -174,9 +185,10 @@ for line_label in labels.keys():
         #     label = 'second order: ' + r'$y = ax^2 + bx + c$'
         popt, pcov = optimize.curve_fit(function, n_homes, time_end)
         coeffs = popt
-        f_fitted = [function(x, *coeffs) for x in list(range(x_max))]
+        xs = list(range(1, x_max))
+        f_fitted = [function(x, *coeffs) for x in xs]
         label = r'$y = $' + f'{coeffs[0]:.2f}' + f'$x^2 $' + f' + {coeffs[1]:.2f}' + r'$x $' + f' + {coeffs[2]:.2f}'
-        plt.plot(list(range(x_max)), f_fitted, label=label, color=colour, linestyle=ls)
+        plt.plot(xs, f_fitted, label=label, color=colour, linestyle=ls)
         print(f"{line_label}: ")
         print(f"times : {time_end}")
         print(f"sum square error {sum((y_fitted - y) ** 2 for y_fitted, y in zip(f_fitted, time_end)):.2e}")
@@ -192,7 +204,7 @@ axs[1].set_xlabel("Number of homes")
 axs[0].legend(fancybox=True)
 axs[1].legend(fancybox=True)
 fig.savefig(
-    f"outputs/results_analysis/facmac_results2.pdf",
+    f"outputs/results_analysis/facmac_results3.pdf",
     bbox_inches='tight', format='pdf', dpi=1200
 )
 plt.close('all')
@@ -280,5 +292,5 @@ for xs, ys, label in zip(
     plt.legend()
     plt.gca().set_yscale('log')
     plt.title(label)
-    fig.savefig(f"outputs/results_analysis/{label}_growth2")
+    fig.savefig(f"outputs/results_analysis/{label}_growth3")
     plt.close('all')
