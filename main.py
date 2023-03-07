f39852b04c4813c6637a896163701404ef84e85c
floracharbo <flora.charbonnier@live.fr>
2023-03-07 10:40:09 +0000

    experiment 10 repeats

--- main.py
+++ main.py
@@ -17,24 +17,24 @@ from src.simulations.runner import run
 # q learning / facmac no traj / facmac traj / facmac supervised no traj / facmac supervised traj
 settings = {
     'RL': {
-        'state_space': ['grdC'] * 1 + [['flexibility', 'grdC_n2']] * 6 * 4,
+        'state_space': ['grdC'] * 1 + [['flexibility', 'grdC_n2']] * 6 * 3,
         'n_epochs': 20,
         'n_repeats': 10,
-        'type_learning': ['q_learning'] * 1 + ['facmac'] * 6 * 4,
-        'trajectory': [False] * 7 + [True] * 6 + [False] * 6 + [True] * 6,
-        'supervised_loss': [False] * 13 + [True] * 6 * 2,
+        'type_learning': ['q_learning'] * 1 + ['facmac'] * 6 * 3,
+        'trajectory': [False] * 7 + [True] * 6 + [False] * 6,
+        'supervised_loss': [False] * 13 + [True] * 6,
         'act_noise': 0.01,
         'lr': 1e-2,
     },
     'syst': {
         'force_optimisation': True,
-        'n_homes': [50] + [1, 3, 5, 10, 20, 50] * 4,
+        'n_homes': [50] + [1, 3, 5, 10, 20, 50] * 3,
     }
 }
 
 # 1 to run simulation, 2 to plot runs in no_runs, 3 plots results vs n_ag
-RUN_MODE = 1
-no_runs = [282]  # if plotting
+RUN_MODE = 2
+no_runs = [515]  # if plotting
 
 run(RUN_MODE, settings, no_runs)
 
