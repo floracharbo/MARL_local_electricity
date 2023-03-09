c233301327b901e87705ccc47fd0cd0fe3af938a
floracharbo <flora.charbonnier@live.fr>
2023-03-09 09:49:03 +0000

    experiment FHO

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ from src.simulations.runner import run
 # q learning / facmac no traj / facmac traj / facmac supervised no traj / facmac supervised traj
 settings = {
     'RL': {
-        'state_space': [['flexibility', 'grdC_n2']],
+        'state_space': [['flexibility', 'grdC_n2']] * 6,
         'n_epochs': 20,
         'n_repeats': 10,
         'type_learning': 'facmac',
-        'trajectory': [True],
-        'supervised_loss': [True],
+        'trajectory': False,
+        'supervised_loss': True,
         'act_noise': 0.01,
         'lr': 1e-2,
     },
     'syst': {
         'force_optimisation': True,
-        'n_homes': [30],
+        'n_homes': [1, 3, 5, 10, 20, 30],
     }
 }
 
