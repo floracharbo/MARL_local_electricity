222545c1276f0fc01064b3e668216179ea2e95f1
floracharbo <flora.charbonnier@live.fr>
2023-03-13 16:57:14 +0000

    comment out v = np.array(v, dtype=float)  # get np.nan from None

--- src/learners/facmac/components/episode_buffer.py
+++ src/learners/facmac/components/episode_buffer.py
@@ -123,7 +123,7 @@ class EpisodeBatch:
                                    f"or episode data")
                 dtype = self.scheme[k].get("dtype", th.float32)
 
-                v = np.array(v, dtype=float)  # get np.nan from None
+                # v = np.array(v, dtype=float)  # get np.nan from None
                 v = th.tensor(v, dtype=dtype, device=self.device)
                 target[k_][_slices] = v.view_as(target[k_][_slices])
                 if k_ in self.preprocess:
