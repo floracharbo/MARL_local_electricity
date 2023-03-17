f406900b62c7461d019022c7524c81b41eb73f44
floracharbo <flora.charbonnier@live.fr>
2023-03-17 11:17:23 +0000

    q_learning experiments

--- main.py
+++ main.py
@@ -17,19 +17,19 @@ from src.simulations.runner import run
 # q learning / facmac no traj / facmac traj / facmac supervised no traj / facmac supervised traj
 settings = {
     'RL': {
-        'state_space': [['flexibility', 'grdC_n2']] * 2,
+        'state_space': 'grdC',
         'n_epochs': 20,
         'n_repeats': 10,
-        'type_learning': 'facmac',
-        'evaluation_methods': 'env_r_c',
-        'trajectory': [True] * 2,
-        'supervised_loss': True,
+        'type_learning': 'q_learning',
+        # 'evaluation_methods': 'env_r_c',
+        # 'trajectory': [True] * 2,
+        # 'supervised_loss': True,
         'act_noise': 0.01,
         'lr': 1e-2,
     },
     'syst': {
         'force_optimisation': True,
-        'n_homes': [20, 30],
+        'n_homes': [1, 3, 5, 10, 20, 30],
     }
 }
 
