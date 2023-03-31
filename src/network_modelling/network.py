1f077a6ce2f31a0bce34841a0d4eacb930a0e4e2
julie-vienne <j.vienne@hotmail.com>
2023-03-31 19:53:29 +0200

    Small correction in loaded buses definition

--- src/network_modelling/network.py
+++ src/network_modelling/network.py
@@ -248,7 +248,7 @@ class Network:
                 self._assign_power_to_load_or_sgen(
                     netq_passive[homeP], self.n_homes + homeP, type='q_mvar')
         pp.runpp(self.net)
-        self.loaded_buses = np.array(self.net.load.bus[self.net.load.p_mw >= 0])
+        self.loaded_buses = np.array(self.net.load.bus[self.net.load.p_mw > 0])
         self.sgen_buses = np.array(self.net.sgen.bus[self.net.sgen.p_mw > 0])
         hourly_line_losses = sum(self.net.res_line['pl_mw']) * 1e3
         hourly_reactive_power_losses = sum(self.net.res_line['ql_mvar']) * 1e3
