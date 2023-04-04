b75f2c0f3eec9e05f0ed3f34d91ada9edb9c860c
floracharbo <flora.charbonnier@live.fr>
2023-04-04 19:39:08 +0100

    Update runner.py

--- src/simulations/runner.py
+++ src/simulations/runner.py
@@ -569,9 +569,9 @@ def run(run_mode, settings, no_runs=None):
 
             settings_i = get_settings_i(settings, i)
             trajectory = settings_i['RL']['trajectory']
-            trajectory = True
             settings_i['RL']['evaluation_methods'] = None if settings_i['RL'][
                                                                'type_learning'] == 'q_learning' else 'env_r_c'
+            trajectory = True
             settings_i['RL']['obs_agent_id'] = False if trajectory else True
             settings_i['RL']['nn_type'] = 'cnn' if trajectory else 'linear'
             settings_i['RL']['rnn_hidden_dim'] = 1e3 if trajectory else 5e2
