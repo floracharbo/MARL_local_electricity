ece544a527233ad7f04bab1e6a974a7181dd7b7f
floracharbo <flora.charbonnier@live.fr>
2023-04-04 15:40:54 +0100

    Create plot_linear_actions.py

---
+++ src/post_analysis/log/plot_linear_actions.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+
+
+green = (117 / 255, 189 / 255, 167 / 255)
+grey = '#999999'
+blue = '#377eb8'  # colorblind blue
+
+fig = plt.figure()
+plt.xlabel(r'$a_{battery}$ [-]')
+plt.ylabel(r'$\Delta s$ [kWh]')
+
+actions = [-1, 0, 1]
+
+# for min(s) < 0 and max(s) < 0
+mins = - 10
+maxs = - 5
+deltas = [mins, maxs, maxs]
+plt.plot(actions, deltas, color=green, label=r'$\min(s) < 0$ and $\max(s) < 0$')
+
+# for min(s) < 0 and max(s) > 0
+mins = - 10
+maxs = 10
+deltas = [mins, 0, maxs]
+plt.plot(actions, deltas, color=grey, label=r'$\min(s) < 0$ and $\max(s) > 0$')
+
+# for min(s) > 0 and max(s) > 0
+mins = 5
+maxs = 10
+deltas = [mins, mins, maxs]
+plt.plot(actions, deltas, color=blue, label=r'$\min(s) > 0$ and $\max(s) > 0$')
+
+ax = plt.gca()
+# ax.set_xticks([])
+# ax.set_yticks([])
+plt.legend()
+fig.savefig(
+    "outputs/results_analysis/linear_battery_action.pdf",
+    bbox_inches='tight', format='pdf', dpi=1200
+)
