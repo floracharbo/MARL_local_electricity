8ea550c58daac98504f6888b6044b49693f6e22f
floracharbo <flora.charbonnier@live.fr>
2023-04-04 15:41:06 +0100

    Update plot_facmac_paper.py

--- src/post_analysis/log/plot_facmac_paper.py
+++ src/post_analysis/log/plot_facmac_paper.py
@@ -7,6 +7,8 @@ import numpy as np
 import pandas as pd
 from scipy import optimize
 
+from src.post_analysis.plotting.plot_rl_performance_metrics import _barplot
+
 # plot timing vs performance for n layers / dim layers; runs 742-656
 font = {'size': 14}
 matplotlib.rc('font', **font)
@@ -17,10 +19,10 @@ labels = {
     'OIQ': 'Optimisation-informed independent Q-learning',
     'MQ': 'Marginal reward Q-learning',
     'OMQ': 'Optimisation-informed, marginal reward Q-learning',
-    'FD': 'FACMAC (day-ahead)',
+    'FD': 'Centralised but factored critic',
     'FDO': 'Optimisation-informed  FACMAC (day-ahead)',
-    'FH': 'FACMAC',  # (hourly)
-    'FHO': 'Optimisation-informed FACMAC (hourly)'
+    # 'FH': 'FACMAC',  # (hourly)
+    # 'FHO': 'Optimisation-informed FACMAC (hourly)'
 }
 type_learning = {
     'IQ': 'env_r_d',
@@ -43,7 +45,10 @@ opt_informed = {
     'FH': False,
     'FHO': True,
 }
-runQ = list(range(534, 539)) + [677],
+# runQ = list(range(534, 539)) + [677]
+# runQ = [534, 535, 536, 537, 538, 677]
+runQ = list(range(1132, 1136)) + list(range(1137, 1138)) + list(range(1166, 1174)) + [1176]
+
 
 runs = {
     # 'OMQ': [170, 94, 96, 97, 98, 99],
@@ -65,12 +70,14 @@ runs = {
     # 'FD': list(range(265, 271)),
     # 'FD': list(range(516, 522)),
     # 'FD': list(range(684, 690)),
-    'FD': list(range(1089, 1091)) + list(range(1092, 1096)),
+    # 'FD': list(range(1089, 1091)) + list(range(1092, 1096)),
+    'FD': list(range(1146, 1152)) + list(range(1158, 1166)),
 
     # 'FDO': list(range(257, 261)) + list(range(263, 265)),
     # 'FDO': list(range(528, 534)),
     # 'FDO': list(range(582, 587)) + [697],
-    'FDO': [1115, 1116, 1119, 1121, 1122, 1123],
+    # 'FDO': [1115, 1116, 1119, 1121, 1122, 1123],
+    'FDO': list(range(1152, 1158)) + [1174, 1175],
     # missing 30
 
     # 'FH': [126, 127, 128, 129, 130, 131],
@@ -105,18 +112,19 @@ blue = '#377eb8'  # colorblind blue
 red = (192 / 255, 0, 0)
 
 colours = {
-    'IQ': blue,
-    'OIQ': blue,
-    'MQ': green,
-    'OMQ': green,
-    'FD': grey,
-    'FDO': grey,
+    'IQ': grey,
+    'OIQ': grey,
+    'MQ': blue,
+    'OMQ': blue,
+    'FD': green,
+    'FDO': green,
     'FH': red,
     'FHO': red,
 }
 
 # n_homes = [1, 3, 5, 10, 20, 50]
-n_homes = [1, 3, 5, 10, 20, 30]
+# n_homes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30]
+n_homes = [1, 5, 10, 15, 20, 25, 30]
 
 results_analysis_path = Path("outputs/results_analysis")
 log_path = results_analysis_path / "log_runs.csv"
@@ -167,11 +175,18 @@ def logx(x, a, b):
 x_max = n_homes[-1]
 ys_time = {entry: [] for entry in compare_times}
 
+reliability_metric_labels = ['DT', 'SRT', 'LRT', 'DR', 'RR']
+reliability_metrics = {line_label: {} for line_label in labels.keys()}
+
 fig, axs = plt.subplots(2, 1, figsize=(8, 10))
 
 axs[0].plot([0, n_homes[-1]], [0, 0], color="gray", label=None, alpha=0.5)
 
 for line_label in labels.keys():
+    reliability_metrics[line_label] = {
+        reliability_metric_label: {}
+        for reliability_metric_label in reliability_metric_labels
+    }
     values = {metric: np.full(len(n_homes), np.nan) for metric in metrics}
     time_end = np.full(len(n_homes), np.nan)
     colour = colours[line_label]
@@ -179,12 +194,27 @@ for line_label in labels.keys():
         runs[line_label] = runs[line_label][0]
     for i, run in enumerate(runs[line_label]):
         if run is not None:
-            for metric in metrics:
-                values[metric][i] = log.loc[
-                    log['run'] == run, f'{metric}_{type_learning[line_label]}'
-                ].values[0]
-                time_end[i] = log.loc[log['run'] == run, 'time_end'].values[0]
+            n_homes_i = log.loc[
+                log['run'] == run, 'n_homes'
+            ].values[0]
+            if n_homes_i in n_homes:
+                values_i = n_homes.index(n_homes_i)
+                for metric in metrics:
+                    values[metric][values_i] = log.loc[
+                        log['run'] == run, f'{metric}_{type_learning[line_label]}'
+                    ].values[0]
+                    time_end[values_i] = log.loc[log['run'] == run, 'time_end'].values[0]
+                print(f"{line_label} n_homes {n_homes_i} run {run} {best_score_type} {values[best_score_type][values_i]}")
+                print(f"{line_label} n_homes {n_homes_i} run {run} time_end {time_end[values_i]}")
 
+    last_run = runs[line_label][-1]
+    reliability_metrics_run = np.load(f"outputs/results/run{last_run}/figures/metrics.npy", allow_pickle=True).item()
+    for reliability_metric_label in reliability_metric_labels:
+        try:
+            reliability_metrics[line_label][reliability_metric_label]['ave'] = reliability_metrics_run[reliability_metric_label]['ave'][type_learning[line_label]]
+            reliability_metrics[line_label][reliability_metric_label]['std'] = reliability_metrics_run[reliability_metric_label]['std'][type_learning[line_label]]
+        except Exception as ex:
+            print(f"ex {ex}")
     ls = '--' if opt_informed[line_label] else '-'
     label = None if opt_informed[line_label] else labels[line_label]
 
@@ -207,21 +237,29 @@ for line_label in labels.keys():
             color=colour
         )
         ys_time[line_label] = time_end
-        function = polynomial2
+        function = polynomial2 \
+            if line_label == 'OMQ' else first_order
         popt, pcov = optimize.curve_fit(function, n_homes, time_end)
         coeffs = popt
-        xs = list(range(1, x_max))
+        xs = list(range(1, x_max + 1))
         f_fitted = [function(x, *coeffs) for x in xs]
-        label = r'$y = $'\
-            + f'{coeffs[0]:.2f}' + r'$x^2 $'\
-            + f' + {coeffs[1]:.2f}' + r'$x $'\
-            + f' + {coeffs[2]:.2f}'
-        plt.plot(xs, f_fitted, label=label, color=colour, linestyle=ls)
+        if line_label == 'OMQ':
+        # if True:
+            label = r'$y = $'\
+                + f'{coeffs[0]:.2f}' + r'$x^2 $'\
+                + f' + {coeffs[1]:.2f}' + r'$x $'\
+                + f' + {coeffs[2]:.2f}'
+        else:
+            label = r'$y = $' \
+                    + f'{coeffs[0]:.1f}' + r'$x$' \
+                    + f' + {coeffs[1]:.1f}'
+        axs[1].plot(xs, f_fitted, label=label, color=colour, linestyle=ls)
         print(f"{line_label}: ")
         print(f"times : {time_end}")
         sum_square_error = sum((y_fitted - y) ** 2 for y_fitted, y in zip(f_fitted, time_end))
         print(f"sum square error {sum_square_error:.2e}")
         print(f"{label} a, b first errors = {np.sqrt(np.diag(pcov))}")
+
 
 axs[0].set_ylabel("Savings [£/home/month]")
 axs[0].axes.xaxis.set_ticklabels([])
@@ -230,7 +268,7 @@ axs[1].set_yscale('log')
 axs[1].set_ylabel("Time [s]")
 axs[1].set_xlabel("Number of homes")
 
-axs[0].legend(fancybox=True)
+axs[0].legend(fancybox=True, loc='lower right')
 axs[1].legend(fancybox=True)
 fig.savefig(
     "outputs/results_analysis/facmac_results3.pdf",
@@ -238,16 +276,78 @@ fig.savefig(
 )
 plt.close('all')
 
-xs_facmac = [1, 3, 5, 10, 20, 50]
-ys_facmac = [26.50374389, 38.85938716, 49.69802213, 73.43057513, 123.11931181, 281.53323126]
-xs_iql = [1, 3, 5, 10, 20, 50]
+
+# figure metrics
+subplots_i_j = {
+                'LRT': [0, 2],
+                'DR': [1, 0],
+                'RR': [1, 1],
+                'SRT': [0, 1],
+                'DT': [0, 0]
+                }
+fig, axs = plt.subplots(2, 3, figsize=(8, 10))
+axs = [
+    plt.subplot2grid((2, 6), (0, 0), 1, 2),
+    plt.subplot2grid((2, 6), (0, 2), 1, 2),
+    plt.subplot2grid((2, 6), (0, 4), 1, 2),
+    plt.subplot2grid((2, 6), (1, 0), 1, 2),
+    plt.subplot2grid((2, 6), (1, 3), 1, 2),
+]
+for i_subplot, reliability_metric_label in enumerate(reliability_metric_labels):
+    bars, err = [
+        [reliability_metrics[line_label][reliability_metric_label][s] for line_label in compare_times]
+        for s in ['ave', 'std']
+    ]
+    # if i_subplot, reliability_metric_label in enumerate(subplots_i_j.keys()):
+        # i, j = subplots_i_j[reliability_metric_label]
+
+    axs[i_subplot] = _barplot(
+        bars, compare_times, None,
+        # error=err,
+        title=reliability_metric_label,
+        ax0=axs[i_subplot],
+        colours=[colours[line_label] for line_label in compare_times],
+        text_labels=False
+    )
+    if i_subplot in [0, 3]:
+        axs[i_subplot].set_ylabel('£/home/h')
+# for i, method in enumerate(compare_times):
+#     axs[1, 2].bar(i + 0.5, [0], label=labels[method], color=colours[method])
+# axs[1, 2].legend(fancybox=True)
+# axs[1, 2].set_yticks([])
+# axs[1, 2].set_xticks([])
+
+plt.tight_layout()
+
+fig.savefig(f"outputs/results_analysis/metrics.pdf", bbox_inches='tight', format='pdf', dpi=1200)
+
+# time facmac
+
+# xs_facmac = [1, 3, 5, 10, 20, 50]
+# ys_facmac = [26.50374389, 38.85938716, 49.69802213, 73.43057513, 123.11931181, 281.53323126]
+# ys_facmac = [
+# 67.54928350448610,
+# 114.3641710281370,
+# 160.202467918396,
+# 274.6239285469060,
+# 490.543824672699,
+# 693.8394634723660
+# ]
+
+# xs_iql = [1, 3, 5, 10, 20, 50]
 # ys_iql = [133.11673403, 302.6728189, 328.64307976, 710.36226296, 1860.68218756, 8911.68623304]
-ys_iql = [132.52754283, 297.46064401, 508.72118497, 1051.24059987, 2497.18453503, 9999.79186893]
-for xs, ys, label in zip(
-    [xs_facmac, xs_iql],
-    [ys_facmac, ys_iql],
-    ['facmac', 'iql']
-):
+# ys_iql = [132.52754283, 297.46064401, 508.72118497, 1051.24059987, 2497.18453503, 9999.79186893]
+# ys_iql = [
+# 810.4848906993870,
+# 1819.6897401809700,
+# 2892.1081290245100,
+# 6172.315644741060,
+# 15143.180866479900,
+# 27013.816419124600
+# ]
+for line_label in compare_times:
+    xs = n_homes
+    ys = ys_time[line_label]
     fig = plt.figure()
     plt.plot(xs, ys, 'o', label='data')
 
@@ -260,17 +360,20 @@ for xs, ys, label in zip(
         'polynomial3', 'unknown_order', 'xlogx'
     ]
     for function, label_func in zip(functions, labels_functions):
-        params, pcov = optimize.curve_fit(function, xs, ys)
-        print(f"{label} params {label_func} = {np.sqrt(np.diag(pcov))}")
-        fitted = [function(x, *params) for x in list(range(1, x_max))]
-        sum_square_error = sum((y_fitted - y) ** 2 for y_fitted, y in zip(fitted, ys))
-        print(f"sum square error {sum_square_error:.2e}")
-        print(f"params {label} {label_func}: {params}")
-        plt.plot(list(range(1, x_max)), fitted,
-                 label=label_func)
+        try:
+            params, pcov = optimize.curve_fit(function, xs, ys)
+            fitted = [function(x, *params) for x in list(range(1, x_max + 1))]
+            sum_square_error = sum((y_fitted - y) ** 2 for y_fitted, y in zip(fitted, ys))
+            print(f"sum square error {sum_square_error:.2e}")
+            print(f"params {line_label} {label_func}: {params}")
+            print(f"variability params {line_label}  {label_func} = {np.sqrt(np.diag(pcov))}")
+            plt.plot(list(range(1, x_max + 1)), fitted,
+                     label=label_func)
+        except Exception as ex:
+            print(f"ex {ex} {line_label} {label_func}")
 
     plt.legend()
     plt.gca().set_yscale('log')
-    plt.title(label)
-    fig.savefig(f"outputs/results_analysis/{label}_growth3")
+    plt.title(line_label)
+    fig.savefig(f"outputs/results_analysis/{line_label}_growth3")
     plt.close('all')
