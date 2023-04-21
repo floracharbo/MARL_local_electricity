bce60defb930023c8c15977183b038b8d04dbdf1
floracharbo <flora.charbonnier@live.fr>
2023-04-21 17:35:01 +0100

    debug all states in env_spaces
    - update bat <- car
    - use hedge.list_clusters, env.hedge.list_factors
    - update max amplitude in subsequent temperature levels
    - update _get_car_tau
    - handle time_step = 24

--- src/utilities/env_spaces.py
+++ src/utilities/env_spaces.py
@@ -57,8 +57,10 @@ def compute_max_car_cons_gen_values(env, state_space):
     max_car_cons, max_normcons, max_normgen, max_car_dem_agg = [-1 for _ in range(4)]
     weekday_types = env.prm["syst"]["weekday_types"]
 
-    if any(descriptor[0: len("bat_cons_")] == "bat_cons_" for descriptor in state_space):
-        max_car_cons = 1
+    if any(descriptor[0: len("car_cons_")] == "car_cons_" for descriptor in state_space):
+        max_car_cons = np.max(
+            [np.max(env.hedge.fs_brackets[transition]) for transition in env.prm['syst']['day_trans']]
+        )
             # np.max(
             # [
             #     [np.max(env.hedge.profs["car"]["cons"][dt][c]) for dt in weekday_types]
@@ -126,7 +128,7 @@ class EnvSpaces:
             "store_bool_flex": self.get_store_bool_flex
         }
 
-        self.cluss, self.factors = env.hedge.clusters, env.hedge.factors
+        self.cluss, self.factors = env.hedge.list_clusters, env.hedge.list_factors
 
     def _init_factors_profiles_parameters(self, prm):
         self.perc = {}
@@ -152,6 +154,7 @@ class EnvSpaces:
             + prm['car']['d_max'] \
             + max_normcons * f_max["loads"] * prm['loads']['flex'][0] * 0.75
         n_clus = prm['n_clus']
+        max_dT = prm["heat"]["Tc"] - prm["heat"]["Ts"] + prm['heat']['dT']
         columns = ["name", "min", "max", "n", "discrete"]
         info = [
             ["None", 0, 0, 1, 1],
@@ -159,13 +162,7 @@ class EnvSpaces:
             ["store0", 0, prm["car"]["caps"], n_other_states, 0],
             ["grdC", min(prm["grd"]["Call"]), max(prm["grd"]["Call"]), n_other_states, 0],
             ["grdC_level", 0, 1, rl["n_grdC_level"], 0],
-            [
-                "dT",
-                - (prm["heat"]["Tc"] - prm["heat"]["Ts"]),
-                prm["heat"]["Tc"] - prm["heat"]["Ts"],
-                n_other_states,
-                0
-            ],
+            ["dT", - max_dT, max_dT, n_other_states, 0],
             [
                 "dT_next",
                 - (prm["heat"]["Tc"] - prm["heat"]["Ts"]),
@@ -183,15 +180,15 @@ class EnvSpaces:
             # clusters - for whole day
             ["loads_clus_step", 0, n_clus['loads'] - 1, n_clus['loads'], 1],
             ["loads_clus_prev", 0, n_clus["loads"] - 1, n_clus["loads"], 1],
-            ["car_clus_step", 0, n_clus["car"] - 1, n_clus["car"], 1],
-            ["car_clus_prev", 0, n_clus["car"] - 1, n_clus["car"], 1],
+            ["car_clus_step", 0, n_clus["car"], n_clus["car"], 1],
+            ["car_clus_prev", 0, n_clus["car"], n_clus["car"], 1],
             # scaling factors - for whole day
             ["loads_fact_step", f_min["loads"], f_max["loads"], n_other_states, 0],
             ["loads_fact_prev", f_min["loads"], f_max["loads"], n_other_states, 0],
             ["gen_fact_step", f_min["gen"], f_max["gen"], n_other_states, 0],
             ["gen_fact_prev", f_min["gen"], f_max["gen"], n_other_states, 0],
-            ["bat_fact_step", f_min["car"], f_max["car"], n_other_states, 0],
-            ["bat_fact_prev", f_min["car"], f_max["car"], n_other_states, 0],
+            ["car_fact_step", f_min["car"], f_max["car"], n_other_states, 0],
+            ["car_fact_prev", f_min["car"], f_max["car"], n_other_states, 0],
             # absolute value at time step / hour
             ["loads_cons_step", 0, max_normcons * f_max["loads"], n_other_states, 0],
             ["loads_cons_prev", 0, max_normcons * f_max["loads"], n_other_states, 0],
@@ -574,16 +571,21 @@ class EnvSpaces:
                         else prm["grd"]["Call"][self.i0_costs + self.N - 1]
 
                 elif len(descriptor) > 9 \
-                        and (descriptor[-9:-5] == "fact"
-                             or descriptor[-9:-5] == "clus"):
+                        and (descriptor[-9: -5] == "fact"
+                             or descriptor[-9: -5] == "clus"):
                     # scaling factors / profile clusters for the whole day
-                    day = (date - prm["syst"]["current_date0_dtm"]).days
+                    day = (date - self.current_date0).days
+                    if time_step == 24:
+                        day -= 1
                     module = descriptor.split("_")[0]  # car, loads or gen
                     index_day = day - \
                         1 if descriptor.split("_")[-1] == "prev" else day
                     index_day = max(index_day, 0)
-                    data = self.factors if descriptor[-9:-5] == "fact" else self.cluss
-                    val = data[home][module][index_day]
+                    data = self.factors if descriptor[-9: -5] == "fact" else self.cluss
+                    try:
+                        val = data[module][home][index_day]
+                    except Exception as ex:
+                        print(ex)
                 else:  # select current or previous hour - step or prev
                     time_step_val = time_step if descriptor[-4:] == "step" else time_step - 1
                     time_step_val = np.max(time_step_val, 0)
@@ -649,11 +651,12 @@ class EnvSpaces:
 
         loads_T, deltaT, _ = \
             self.car.next_trip_details(time_step, date, home)
-
-        if loads_T is not None and deltaT > 0:
-            val = ((loads_T - res["store"][home][time_step]) / deltaT)
+        time_step_ = self.N - 1 if time_step == self.N else time_step
+        current_store = res["store"][home][time_step_]
+        if loads_T is not None and loads_T > current_store and deltaT > 0:
+            val = ((loads_T - current_store) / deltaT)
         else:
-            val = - 1
+            val = 0
 
         return val
 
@@ -692,24 +695,34 @@ class EnvSpaces:
             descriptor_info = self.space_info.loc[
                 self.space_info['name'] == self.descriptor_for_info_lookup(descriptor)
             ]
-            max_home = descriptor_info['max'].values.item()[home] \
-                if isinstance(descriptor_info['max'].values.item(), list) \
-                else descriptor_info['max'].values.item()
-            min_val = descriptor_info['min'].values.item()
-            normalised_val = (val - min_val) / (max_home - min_val)
+            if (
+                    descriptor[0: 3] == 'gen'
+                    and isinstance(descriptor_info['max'].values.item(), list)
+                    and len(descriptor_info['max'].values.item()) == 12
+            ):
+                max_val = descriptor_info['max'].values.item()[self.current_date0.month - 1]
+                min_val = descriptor_info['min'].values.item()[self.current_date0.month - 1]
+            else:
+                max_val = descriptor_info['max'].values.item()[home] \
+                    if isinstance(descriptor_info['max'].values.item(), (list, np.ndarray)) \
+                    else descriptor_info['max'].values.item()
+                min_val = descriptor_info['min'].values.item()[home] \
+                    if isinstance(descriptor_info['min'].values.item(), (list, np.ndarray)) \
+                    else descriptor_info['min'].values.item()
+            normalised_val = (val - min_val) / (max_val - min_val)
             if abs(normalised_val) < 1e-5:
                 normalised_val = 0
             if not (0 <= normalised_val <= 1):
                 print(
                     f"val {val} normalised_val {normalised_val} "
-                    f"max_home {max_home} descriptor {descriptor}"
+                    f"max_home {max_val} descriptor {descriptor}"
                 )
                 if abs(normalised_val) < abs(normalised_val - 1):
                     normalised_val = 0
                 else:
                     normalised_val = 1
             assert 0 <= normalised_val <= 1, \
-                f"val {normalised_val} max_home {max_home} descriptor {descriptor}"
+                f"val {normalised_val} max_home {max_val} descriptor {descriptor}"
         else:
             normalised_val = val
 
