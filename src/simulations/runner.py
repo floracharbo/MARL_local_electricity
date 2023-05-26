e75a5f865d7d347515ef53f9f28ebda619a0cf0a
floracharbo <flora.charbonnier@live.fr>
2023-05-26 17:11:33 +0100

    evaluation_methods not necessarily none if not trajectory

--- src/simulations/runner.py
+++ src/simulations/runner.py
@@ -577,11 +577,10 @@ def run(run_mode, settings, no_runs=None):
             settings_i = get_settings_i(settings, i)
             if 'trajectory' in settings_i['RL']:
                 trajectory = settings_i['RL']['trajectory']
-                settings_i['RL']['evaluation_methods'] = None \
-                    if settings_i['RL']['type_learning'] == 'q_learning' \
-                    else 'env_r_c'
+                if trajectory:
+                    settings_i['RL']['evaluation_methods'] = 'env_r_c'
                 settings_i['RL']['obs_agent_id'] = False if trajectory else True
-                settings_i['RL']['nn_type'] = 'cnn' if trajectory else 'linear'
+                # settings_i['RL']['nn_type'] = 'cnn' if trajectory else 'linear'
                 settings_i['RL']['rnn_hidden_dim'] = 1e3 if trajectory else 5e2
                 settings_i['RL']['optimizer'] = 'rmsprop' if trajectory else 'adam'
 
