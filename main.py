2b69d35d85acf8bd2cc63a109caaf3b82873e48b
floracharbo <flora.charbonnier@live.fr>
2023-07-03 18:04:28 +0100

    update path to environment.experiment_manager

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Created on Sat Feb  1 15:14:20 2020.
 # # packages
 # =============================================================================
 
-from src.simulations.runner import run
+from src.environment.experiment_manager.runner import run
 
 # Enter experiment-specific settings in the dictionary below if using different parameters
 # to the default parameters in config_files/default_input_parameters, using the example syntax below.
@@ -18,18 +18,32 @@ from src.simulations.runner import run
 
 settings = {
     'RL': {
-       'state_space': [['grdC_n2', 'flexibility'], ['grdC']],
+       'state_space': [['grdC_n2', 'flexibility']] * (4 + 3 + 3 + 1 + 2 + 2 + 2 + 2),
        'n_epochs': 20,
-       'n_repeats': 3,
-       'type_learning': ['facmac'] * 2,
-       'evaluation_methods': [['env_r_c', 'opt_r_c']] * 2,
-       'facmac': {
-           'hysteretic': True,
+       'type_learning': 'q_learning',
+        'act_noise': 0.01,
+        'facmac': {
+            'batch_size': 10,
         },
+        'lr': [1e-3] * 4 + [1e-2, 1e-3, 1e-4] + [1e-3] * (3 + 1 + 2 + 2 + 2 + 2),
+        'n_repeats': 5,
+       # 'evaluation_methods': 'env_r_c',
+        'n_discrete_actions': [3] * (4 + 3 + 3 + 1 + 2) + [2, 10] + [3] * (2 + 2),
+        'n_grdC_level': [3] * (4 + 3 + 3 + 1 + 2 + 2) + [2, 10] + [3] * 2,
+        'n_other_states': [3] * (4 + 3 + 3 + 1 + 2 + 2 + 2) + [2, 10],
+        'aggregate_actions': [False] * (4 + 3 + 3 + 1 + 2 + 2 + 2 + 2) + [True],
+        'q_learning': {
+            'gamma': [0.5, 0.7, 0.9, 0.99] + [0.7] * (3 + 3 + 1 + 2 + 2 + 2 + 2),
+            'eps': [0.5] * (4 + 3) + [0.2, 0.8, 0.9] + [0.5] * (1 + 2 + 2 + 2 + 2),
+            'hysteretic': [False] * (4 + 3 + 3) + [True] + [False] * (2 + 2 + 2 + 2),
+            'beta_to_alpha': [0.5] * (4 + 3 + 3 + 1) + [0.1, 0.9] + [0.5] * (2 + 2 + 2),
+        }
     },
     'syst': {
-       'n_homes': 10,
-    }
+       'n_homes': 30,
+        'clus_dist_share': 0.999,
+        'f0': {'gen': 8.012, 'loads': 9.459, 'car': 8.893}
+    },
 }
 
 # 1 to run simulation, 2 to plot runs in no_runs, 3 plots results vs n_ag
