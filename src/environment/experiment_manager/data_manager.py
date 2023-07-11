2b4de9df1ddb2a71c701cf889423ec805ad6175c
floracharbo <flora.charbonnier@live.fr>
2023-07-11 23:06:16 +0100

    opt_res_file = no

--- src/environment/experiment_manager/data_manager.py
+++ src/environment/experiment_manager/data_manager.py
@@ -265,7 +265,7 @@ class DataManager:
 
         res_name = \
             f"res_P{int(self.seed['P'])}_" \
-            f"{int(active_seed)}{get_opt_res_file(self.prm, evaluation)['opt_res_file']}c"
+            f"{int(active_seed)}{self.prm['paths']['opt_res_file_no']}"
 
         return res_name
 
@@ -394,7 +394,7 @@ class DataManager:
     def file_id(self, evaluation):
         """Generate string to identify the run in saved files."""
         return f"{int(self.seed[self.ext])}{self.ext}" \
-               f"{get_opt_res_file(self.prm, evaluation)['opt_res_file']}"
+               f"{self.prm['paths']['opt_res_file_no']}"
 
     def _loop_replace_data(
             self,
