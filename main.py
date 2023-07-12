010221e2877cd10498b902a53f7d0dcd75c45026
floracharbo <flora.charbonnier@live.fr>
2023-07-12 10:40:21 +0100

    1000 homes facmac

--- main.py
+++ main.py
@@ -18,17 +18,31 @@ from src.environment.experiment_manager.runner import run
 
 settings = {
     'RL': {
-        'type_learning': 'q_learning',
+        'type_learning': 'facmac',
+        # 'evaluation_methods': 'env_r_c',
+        # 'q_learning': {'alpha': [1e-3, 1e-1]}
     },
     'syst': {
-        'n_homes': [1, 2, 3, 5, 10, 20, 30],
-        'force_optimisation': True,
+        'n_homes': 1000,
+        # 'force_optimisation': True,
     },
 }
+# n = 16
+# settings['RL']['n_grdC_level'] = [3 for _ in range(n)]
+# settings['RL']['n_grdC_level'][0: 4] = [1, 3, 5, 20]
+# settings['RL']['eps'] = [0.1 for _ in range(n)]
+# settings['RL']['eps'][4: 7] = [0.01, 0.5, 0.9]
+# settings['RL']['lr'] = [1e-4 for _ in range(n)]
+# settings['RL']['lr'][7: 11] = [1e-5, 1e-3, 1e-2, 1e-1]
+# settings['RL']['initialise_q'] = ['zeros' for _ in range(n)]
+# settings['RL']['initialise_q'][11] = 'random'
+# settings['RL']['gamma'] = [0.7 for _ in range(n)]
+# settings['RL']['gamma'][12: 14] = [0.1, 0.9]
+# settings['RL']['beta_to_alpha'] = [0.5 for _ in range(n)]
+# settings['RL']['beta_to_alpha'][14: 16] = [0.1, 0.9]
 
-
-# 1 to run simulation, 2 to plot runs in no_runs, 3 plots results vs n_ag
+# 1 to run simulation, 2 to plot runs in no_runs
 RUN_MODE = 1
-no_runs = [823]  # if plotting
+no_runs = [1216]  # if plotting
 
 run(RUN_MODE, settings, no_runs)
