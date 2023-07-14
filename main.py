8b76317551ab24076cc57abe6812a9f3987e64f2
floracharbo <flora.charbonnier@live.fr>
2023-07-14 19:18:36 +0300

    tidy main

--- main.py
+++ main.py
@@ -18,35 +18,17 @@ from src.environment.experiment_manager.runner import run
 
 settings = {
     'RL': {
-        'type_learning': ['q_learning'] * 7 + ['facmac'] * (4 * 2),
-        'supervised_loss': [False] * 7 + [True] * 4 + [False] * 4,
-        # 'n_epochs': 20,
-        # 'n_repeats': 10,
+        'type_learning': 'facmac',
     },
     'syst': {
-        'n_homes': [2, 3, 4, 15, 20, 25, 30] + [3, 4, 15, 25] * 2,
-        # 'n_homesP': 45,
+        'n_homes': 20,
     },
-    # 'grd': {
-    #     'simulate_panda_power_only': True,
-    # }
+
 }
-# n = 16
-# settings['RL']['n_grdC_level'] = [3 for _ in range(n)]
-# settings['RL']['n_grdC_level'][0: 4] = [1, 3, 5, 20]
-# settings['RL']['eps'] = [0.1 for _ in range(n)]
-# settings['RL']['eps'][4: 7] = [0.01, 0.5, 0.9]
-# settings['RL']['lr'] = [1e-4 for _ in range(n)]
-# settings['RL']['lr'][7: 11] = [1e-5, 1e-3, 1e-2, 1e-1]
-# settings['RL']['initialise_q'] = ['zeros' for _ in range(n)]
-# settings['RL']['initialise_q'][11] = 'random'
-# settings['RL']['gamma'] = [0.7 for _ in range(n)]
-# settings['RL']['gamma'][12: 14] = [0.1, 0.9]
-# settings['RL']['beta_to_alpha'] = [0.5 for _ in range(n)]
-# settings['RL']['beta_to_alpha'][14: 16] = [0.1, 0.9]
+
 
 # 1 to run simulation, 2 to plot runs in no_runs
 RUN_MODE = 1
-no_runs = [1240]  # if plotting
+no_runs = [1350]  # if plotting
 
 run(RUN_MODE, settings, no_runs)
